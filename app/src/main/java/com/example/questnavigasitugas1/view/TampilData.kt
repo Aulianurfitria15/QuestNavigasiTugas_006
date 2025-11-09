4c1a3e24bba30d5c6712d1acbcf6081869722a7f
Aulianurfitria15 <aulianurfitria17@gmail.com>
2025-11-09 14:30:12 +0700

    nambah data contoh karyawan di listPeserta buat tampilan awal

--- app/src/main/java/com/example/questnavigasitugas1/view/TampilData.kt
+++ app/src/main/java/com/example/questnavigasitugas1/view/TampilData.kt
@@ -13,4 +13,52 @@ data class TampilData(
     val status: String = ""
 )
 
-}
+class PesertaViewModel : ViewModel() {
+
+    private val _listPeserta = MutableStateFlow(
+        listOf(
+            TampilData(
+                namaLengkap = "Gita Anggraini",
+                jenisKelamin = "Perempuan",
+                umur = "40",
+                jabatan = "Security",
+                status = "Aktif"
+            ),
+            TampilData(
+                namaLengkap = "Farhan Yusuf",
+                jenisKelamin = "Laki-Laki",
+                umur = "30",
+                jabatan = "Manager",
+                status = "Cuti"
+            ),
+            TampilData(
+                namaLengkap = "Citra Lestari",
+                jenisKelamin = "Perempuan",
+                umur = "25",
+                jabatan = "Finance",
+                status = "Aktif"
+            ),
+            TampilData(
+                namaLengkap = "Hari Wijaya",
+                jenisKelamin = "Laki-Laki",
+                umur = "27",
+                jabatan = "Staff IT Support",
+                status = "Kontrak"
+            ),
+            TampilData(
+                namaLengkap = "Ayu",
+                jenisKelamin = "Perempuan",
+                umur = "25",
+                jabatan = "Finance",
+                status = "Aktif"
+        ),
+        TampilData(
+                namaLengkap = "Wildan",
+                jenisKelamin = "Laki-Laki",
+                umur = "27",
+                jabatan = "Staff IT Support",
+                status = "Kontrak"
+        )
+        )
+    )
+
