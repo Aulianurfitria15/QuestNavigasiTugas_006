33cf1ceb813785e007819f0da582b89ff5d93c9e
Aulianurfitria15 <aulianurfitria17@gmail.com>
2025-11-09 15:28:56 +0700

    merubah beberapa unutk menyesuaikan tampilan

--- app/src/main/java/com/example/questnavigasitugas1/view/TampilData.kt
+++ app/src/main/java/com/example/questnavigasitugas1/view/TampilData.kt
@@ -9,7 +9,7 @@ data class TampilData(
     val namaLengkap: String = "",
     val jenisKelamin: String = "",
     val umur: String = "",
-    val jabatan: String = "",
+    val pekerjaan: String = "",
     val status: String = ""
 )
 
@@ -18,45 +18,45 @@ class PesertaViewModel : ViewModel() {
     private val _listPeserta = MutableStateFlow(
         listOf(
             TampilData(
-                namaLengkap = "Gita Anggraini",
+                namaLengkap = "Yasmin Dwi",
                 jenisKelamin = "Perempuan",
                 umur = "40",
-                jabatan = "Security",
+                pekerjaan = "Security",
                 status = "Aktif"
             ),
             TampilData(
-                namaLengkap = "Farhan Yusuf",
+                namaLengkap = "Yusuf Maulana",
                 jenisKelamin = "Laki-Laki",
                 umur = "30",
-                jabatan = "Manager",
+                pekerjaan = "Manager",
                 status = "Cuti"
             ),
             TampilData(
-                namaLengkap = "Citra Lestari",
+                namaLengkap = "Sari Mulia",
                 jenisKelamin = "Perempuan",
                 umur = "25",
-                jabatan = "Finance",
+                pekerjaan = "Finance",
                 status = "Aktif"
             ),
             TampilData(
-                namaLengkap = "Hari Wijaya",
+                namaLengkap = "Putra Ahmad",
                 jenisKelamin = "Laki-Laki",
                 umur = "27",
-                jabatan = "Staff IT Support",
+                pekerjaan = "Staff IT Support",
                 status = "Kontrak"
             ),
             TampilData(
                 namaLengkap = "Ayu",
                 jenisKelamin = "Perempuan",
                 umur = "25",
-                jabatan = "Finance",
+                pekerjaan = "Finance",
                 status = "Aktif"
         ),
         TampilData(
                 namaLengkap = "Wildan",
                 jenisKelamin = "Laki-Laki",
                 umur = "27",
-                jabatan = "Staff IT Support",
+                pekerjaan = "Staff IT Support",
                 status = "Kontrak"
         )
         )
